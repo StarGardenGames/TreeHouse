6dd3f6b22673fb68db587ea934ab0c5a1e03e269
BuildTools <unconfigured@null.spigotmc.org>
2015-02-03 19:06:40 -0500

    Implemented camera flipping
    I tweaked some things from how I originally described it. I think it's
    better now though. Also, I get what you were saying now Arend about how
    your camera script worked, and I changed it back to how you had it. I
    think I was just slightly confused by the default speedFactor being 0.
    Also, KNOWN ISSUES: the abrupt switch from perspective to ortho on the
    camera looks weird. I'm not sure what the best thing to do about that
    is.

---
+++ SuperPerspective/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+	
+	public static CameraControl instance;
+	public Transform mount;
+	public float speedFactor;
+	public bool locked = false, is3D = false;
+	private float t;
+	private Transform pcam, ocam;
+	
+	
+	void Awake(){
+		//singleton pattern
+		if( instance == null)
+			instance = this;
+		else if( instance != this)
+			Destroy(gameObject);
+	}
+
+	void Start() {
+		//Get 2D and 3D camera positions
+		pcam = transform.parent.FindChild("CamPosPersp");
+		ocam = transform.parent.FindChild("CamPosOrtho");
+	}
+
+	void LateUpdate () {
+		//update position & orientation
+		if(!locked){
+			transform.position = Vector3.Lerp(transform.position,mount.position, speedFactor);
+			transform.rotation = Quaternion.Slerp(transform.rotation, mount.rotation, speedFactor);
+		}else{
+			//fix position
+			Snap();
+		}
+		
+		//update locked status
+		if(Vector3.Distance(transform.position, mount.position) < .1f) {
+			locked = true;
+			//if 2D switch to ortho when the camera is in place
+			if (!is3D)
+				Camera.main.orthographic = true;
+		}
+	}
+	
+	public void SetMount(Transform newMount){
+		mount = newMount;
+		locked = false;
+		t = 0;
+	}
+	
+	public void Snap(){
+		transform.position = mount.position;
+		transform.rotation = mount.rotation;
+	}
+
+	public void Flip() {
+		is3D = !is3D;
+		if (is3D) {
+			SetMount(pcam);
+			Camera.main.orthographic = false;
+		} else {
+			SetMount(ocam);
+		}
+	}
+
+	public bool IsFlipping() {
+		return !locked;
+	}
+}
---
+++ SuperPerspective/Assets/Scripts/PerspectiveShift.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerspectiveShift : MonoBehaviour {
+
+	public static bool is3D = false;
+	
+
+	void Update () {
+		//Tell the camera to the flip when flip is pressed
+		if (Input.GetButtonDown("Flip") && !CameraControl.instance.IsFlipping()) {
+			CameraControl.instance.Flip();
+		}
+	}
+}
