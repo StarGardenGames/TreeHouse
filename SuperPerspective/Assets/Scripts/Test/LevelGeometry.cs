0d8bfc6761d2162c2d9e54738d49b8c54ce0b931
Cory <cory@th.ru.st>
2015-02-09 00:22:52 -0500

    Created script for level geometry to adjust colliders, added Singleton and PersistentSingleton scripts to SuperPerspective namespace.

---
+++ SuperPerspective/Assets/Scripts/Test/LevelGeometry.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGeometry : MonoBehaviour {
+
+    private BoxCollider boxCollider;
+    private Vector3 colliderSize;
+    private Vector3 colliderCenter;
+    private float ratio;
+
+    void Awake()
+    {
+        // Get the BoxCollider component
+        boxCollider = GetComponent<BoxCollider>();
+
+        // Compute size ratio of my transform and parent's
+        ratio = transform.parent.lossyScale.z / transform.lossyScale.z;
+
+        // Store the normal collider bounds and center
+        colliderSize = boxCollider.bounds.extents / 2;
+        colliderCenter = Vector3.zero;
+    }
+	
+	void Start () 
+    {
+        // Register to perspective shift event
+        ShiftTester.instance.perspectiveShiftEvent += AdjustCollider;
+	}
+
+    private void AdjustCollider(PerspectiveType p)
+    {
+        if (p == PerspectiveType.p2D)
+        {
+            Vector3 parentSize = transform.parent.collider.bounds.extents;
+            boxCollider.size = new Vector3(colliderSize.x, colliderSize.y, parentSize.z);
+        }
+        else if (p == PerspectiveType.p3D)
+        {
+            boxCollider.size = colliderSize;
+            boxCollider.center = colliderCenter;
+        }
+    }
+}
---
+++ SuperPerspective/Assets/Scripts/Test/ShiftTester.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using SuperPerspective.Singleton;
+
+public class ShiftTester : Singleton<ShiftTester>
+{
+
+    public event System.Action<PerspectiveType> perspectiveShiftEvent;
+    private PerspectiveType p = PerspectiveType.p3D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (perspectiveShiftEvent != null)
+            {
+                if (p == PerspectiveType.p2D)
+                {
+                    perspectiveShiftEvent(PerspectiveType.p3D);
+                    p = PerspectiveType.p3D;
+                }
+                else
+                {
+                    perspectiveShiftEvent(PerspectiveType.p2D);
+                    p = PerspectiveType.p2D;
+                }
+            }
+        }
+    }
+}
+
+public enum PerspectiveType
+{
+    p2D, p3D
+}
+
+
