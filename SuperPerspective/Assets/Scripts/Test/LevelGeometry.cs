9acc5be7857fb7b387f09ddaa55c0a990f46cee0
Cory <cory@th.ru.st>
2015-02-13 11:26:55 -0500

    Fixed collider stretching to fit the proper center and dimensions.

--- SuperPerspective/Assets/Scripts/Test/LevelGeometry.cs
+++ SuperPerspective/Assets/Scripts/Test/LevelGeometry.cs
@@ -8,12 +8,18 @@ public class LevelGeometry : MonoBehaviour {
     private BoxCollider boxCollider;
     private Vector3 colliderSize;
     private Vector3 colliderCenter;
+    private float zScaleRatioParent;
+    private float zScaleRatioWorld;
     private float ratio;
 
     void Awake()
     {
         // Get the BoxCollider component
         boxCollider = GetComponent<BoxCollider>();
+
+        // Store the ratio of our scale to the parent's
+        zScaleRatioParent = parentPlatform.transform.lossyScale.z / transform.lossyScale.z;
+        zScaleRatioWorld = (1 / transform.lossyScale.z);
 
         // Store the normal collider bounds and center
         colliderSize = new Vector3(1f, 1f, 1f);
@@ -30,8 +36,8 @@ public class LevelGeometry : MonoBehaviour {
     {
         if (p == PerspectiveType.p2D)
         {
-            Vector3 parentSize = parentPlatform.collider.bounds.extents;
-            boxCollider.size = new Vector3(colliderSize.x, colliderSize.y, parentSize.z);
+            boxCollider.center = new Vector3(colliderCenter.x, colliderCenter.y, (parentPlatform.transform.position.z - transform.position.z) * zScaleRatioWorld);
+            boxCollider.size = new Vector3(colliderSize.x, colliderSize.y, zScaleRatioParent);
         }
         else if (p == PerspectiveType.p3D)
         {
