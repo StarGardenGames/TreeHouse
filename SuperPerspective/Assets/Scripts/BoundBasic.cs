ceb37f85742cb880a7a43a97eb8d3e4cff18642a
Arend Peter <apc1993@gmail.com>
2015-02-14 23:09:44 -0500

    Added Bounding
    Just the basic bounding script which was also used in the previous
    perspective.
    
    I meant to add it earlier, sorry

---
+++ SuperPerspective/Assets/Scripts/BoundBasic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundBasic : MonoBehaviour {
+	
+	Rect bounds;
+	bool left=true,right=true,front=true,back=true;//probably won't need this
+	// Use this for initialization
+	void Start () {
+		GameObject grnd=GameObject.Find ("Ground");
+		float gX=grnd.transform.position.x;
+		float gZ=grnd.transform.position.z;
+		float gW=grnd.transform.lossyScale.x;
+		float gD=grnd.transform.lossyScale.z;
+		float pW=transform.lossyScale.x;
+		float pD=transform.lossyScale.z;
+		bounds = new Rect (gX - (gW / 2f)+(pW/2f), gZ - (gD / 2f)+(pD/2f), gW - pW, gD - pD);
+	}
+	// Update is called once per frame
+	void LateUpdate () {
+		//Note to self
+		//Why not : transform.x = Mathf.Max (bounds.xMin, Mathf.Min (bounds.xMax, transform.position.x));
+		Vector3 pos = transform.position;
+		if(front) pos.x = Mathf.Min (bounds.xMax, pos.x);
+		if(back) pos.x = Mathf.Max (bounds.xMin, pos.x);
+		if(right) pos.z = Mathf.Max (bounds.yMin, pos.z);
+		if(left) pos.z = Mathf.Min (bounds.yMax, pos.z);
+		transform.position = pos;
+	}
+}
