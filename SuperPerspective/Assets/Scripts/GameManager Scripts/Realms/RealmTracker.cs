a6cac3370e21f4c9d7c281ffe47369e5023b0572
Arend Peter <apc1993@gmail.com>
2015-06-07 21:49:36 -0400

    Added Levels to Testing World & Adjusted Realms
    Testing world now includes the following levels
    -edge grabbing
    -hub
    -blank level
    -realms
    
    Realms have been adjusted, you now have to include a realm tracker
    within the level to keep track of the red & blue blocks.

---
+++ SuperPerspective/Assets/Scripts/GameManager Scripts/Realms/RealmTracker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RealmTracker : Activatable {
+
+	public GameObject[] reds,blues;
+	
+	public override void setActivated(bool a){
+		RealmManager.instance.toggle(a);
+	}
+}
