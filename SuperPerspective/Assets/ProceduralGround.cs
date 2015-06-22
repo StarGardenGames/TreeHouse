f24916cc848c353684b590ef0c17cc054fd289b9
neonwednesdays <dxiao.ns@gmail.com>
2015-06-21 21:40:07 -0400

    Ground crack prefabs

---
+++ SuperPerspective/Assets/ProceduralGround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralGround : MonoBehaviour {
+
+	public GameObject objectToRandomize;
+	public bool rotateX, rotateY, rotateZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
