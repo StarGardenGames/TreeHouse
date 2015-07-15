13f53841c73580941b9b8a44db3e06bbfc32ea31
Peter <paytar1000@gmail.com>
2015-07-15 13:58:01 -0400

    Thin wall and grass prototype
    Implemented the thin wall mechanic. Also tried to do Daniel's job for
    him and make grass with marginal success.

---
+++ SuperPerspective/Assets/Scripts/Grass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.Rotate(Vector3.up, Mathf.Rad2Deg * transform.position.x * transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
