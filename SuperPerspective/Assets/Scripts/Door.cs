88051540296c6f5b930bac5582e7e0a80155898e
nshooter3 <nick.shooter@gmail.com>
2015-07-19 20:15:22 -0400

    Audio additions, new level
    Added sound and prefabs for shifting, ice, and doors.

--- SuperPerspective/Assets/Scripts/Door.cs
+++ SuperPerspective/Assets/Scripts/Door.cs
@@ -8,6 +8,7 @@ public class Door : ActiveInteractable {
 	public string destName;
 	Door destDoor;
 	public Color particleColor;
+	public AudioSource warp;
 	
 	public void Awake(){
 		//update particle color
@@ -15,12 +16,28 @@ public class Door : ActiveInteractable {
 		p.startColor = particleColor;
 		p.Simulate(2f);
 		p.Play();
+
+		//Adding in reference to door mesh, where I added in the warp sound -Nick
+		AudioSource[] comps = GetComponentsInChildren<AudioSource>();
+		foreach (AudioSource comp in comps)
+		{
+			if ( comp.gameObject.name == "DoorMesh" )
+			{
+				warp = comp;
+				warp.volume = 0.35f;
+				break;
+			}
+		}
+		//Debug.Log (warp);
 	}
 
 	public override void Triggered(){
-		if(destDoor!=null)
-			player.GetComponent<PlayerController>().Teleport(
-				destDoor.transform.position + new Vector3(0,0,-2));
+		if (destDoor != null) {
+			player.GetComponent<PlayerController> ().Teleport (
+				destDoor.transform.position + new Vector3 (0, 0, -2));
+			//Playing warp sound -Nick
+			warp.Play ();
+		}
 		else
 			Debug.Log("Door not linked");
 	}
--- SuperPerspective/Assets/Scripts/ShiftSound.cs
+++ SuperPerspective/Assets/Scripts/ShiftSound.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class ShiftSound : MonoBehaviour {
 
+	AudioSource source;
+
 	// Use this for initialization
 	void Start () {
-		GameStateManager.instance.PerspectiveShiftEvent += PlayShiftAudio;
+		GameStateManager.instance.PerspectiveShiftSuccessEvent += PlayShiftAudio;
+		GameStateManager.instance.PerspectiveShiftFailEvent += PlayFailAudio;
+		source = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -13,15 +17,24 @@ public class ShiftSound : MonoBehaviour {
 	
 	}
 
-	void PlayShiftAudio(PerspectiveType p){
+	void PlayShiftAudio(){
+
+		PerspectiveType p = GameStateManager.instance.currentPerspective;
 
 		if (p == PerspectiveType.p3D) {
-
+			source.clip = Resources.Load ("Sound/SFX/Player/Shift/To3D")  as AudioClip;
+			source.Play();
 		}
 
 		else if (p == PerspectiveType.p2D) {
-
+			source.clip = Resources.Load ("Sound/SFX/Player/Shift/To2D")  as AudioClip;
+			source.Play();
 		}
 
+	}
+
+	void PlayFailAudio(){
+		source.clip = Resources.Load ("Sound/SFX/Player/Shift/ShiftFail")  as AudioClip;
+		source.Play();
 	}
 }
