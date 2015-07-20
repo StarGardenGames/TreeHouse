dcb19ef97f55788b2dd48930dcc921b5920632da
nshooter3 <nick.shooter@gmail.com>
2015-07-19 22:22:33 -0400

    Revert "Audio additions, new level"
    This reverts commit 88051540296c6f5b930bac5582e7e0a80155898e.

--- SuperPerspective/Assets/Scripts/Door.cs
+++ SuperPerspective/Assets/Scripts/Door.cs
@@ -8,7 +8,6 @@ public class Door : ActiveInteractable {
 	public string destName;
 	Door destDoor;
 	public Color particleColor;
-	public AudioSource warp;
 	
 	public void Awake(){
 		//update particle color
@@ -16,28 +15,12 @@ public class Door : ActiveInteractable {
 		p.startColor = particleColor;
 		p.Simulate(2f);
 		p.Play();
-
-		//Adding in reference to door mesh, where I added in the warp sound -Nick
-		AudioSource[] comps = GetComponentsInChildren<AudioSource>();
-		foreach (AudioSource comp in comps)
-		{
-			if ( comp.gameObject.name == "DoorMesh" )
-			{
-				warp = comp;
-				warp.volume = 0.35f;
-				break;
-			}
-		}
-		//Debug.Log (warp);
 	}
 
 	public override void Triggered(){
-		if (destDoor != null) {
-			player.GetComponent<PlayerController> ().Teleport (
-				destDoor.transform.position + new Vector3 (0, 0, -2));
-			//Playing warp sound -Nick
-			warp.Play ();
-		}
+		if(destDoor!=null)
+			player.GetComponent<PlayerController>().Teleport(
+				destDoor.transform.position + new Vector3(0,0,-2));
 		else
 			Debug.Log("Door not linked");
 	}
