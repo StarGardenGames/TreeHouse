3eef7144550c8cb64efab15c76f555d908287b64
neonwednesdays <dxiao.ns@gmail.com>
2015-07-19 22:06:12 -0400

    Daniels week 1 level

---
+++ SuperPerspective/Assets/Scripts/BackgroundObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject me = this.gameObject;
+		Color tint = new Color(0, 0.5f, 1f, 0.8f);
+		if(!PlayerController.instance.is3D()){
+			Renderer[] rs = me.GetComponentsInChildren<Renderer>();
+			foreach(Renderer r in rs){
+				Material m = r.material;
+				Color c = new Color(m.color.r, m.color.g, m.color.b, 0.5f);
+				//m.color = c;
+
+				m.SetColor ("_TintColor", tint);
+
+				print(m.color);
+			}
+		}
+	}
+}
