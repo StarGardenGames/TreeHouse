87dd0f63dd0764bace4701201e2a5b246fcf12fa
nshooter3 <nick.shooter@gmail.com>
2015-08-27 04:06:05 -0400

    Additional Ice levels, tweaks to older levels

---
+++ SuperPerspective/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5 * Random.Range (0.8f, 1.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(speed*Time.deltaTime, speed*Time.deltaTime, speed*Time.deltaTime));
+	}
+}
