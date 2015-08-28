cb149aa831f46943ab654266c694aef1d551f421
neonwednesdays <dxiao.ns@gmail.com>
2015-08-28 03:36:34 -0400

    fixed main light controller prefab

---
+++ SuperPerspective/Assets/Scripts/LightingController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class LightingController : MonoBehaviour {
+
+    public bool fog;
+    public Color fogColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    public FogMode fogMode = FogMode.ExponentialSquared;
+    public float fogDensity = 0.01f;
+
+    public float linearFogStart = 0.0f;
+    public float linearFogEnd = 300.0f;
+
+    public Color ambientLight = new Color(0.2f, 0.2f, 0.2f, 1.0f);
+    public float ambientIntensity = 0.0f;
+    public Material skyboxMaterial;
+
+    public float reflectionIntensity = 0.4f;
+
+    public float haloStrength = 0.5f;
+    public float flareStrength = 1.0f;
+
+    void Awake() {
+        RenderSettings.fog = fog;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogMode = fogMode;
+        RenderSettings.fogDensity = fogDensity;
+
+        RenderSettings.fogStartDistance = linearFogStart;
+        RenderSettings.fogEndDistance = linearFogEnd;
+
+        //RenderSettings.ambientLight = ambientLight;
+        RenderSettings.ambientIntensity = ambientIntensity;
+        RenderSettings.skybox = skyboxMaterial;
+        RenderSettings.reflectionIntensity = reflectionIntensity;
+
+        //RenderSettings.haloStrength = haloStrength;
+        //RenderSettings.flareStrength = flareStrength;
+    }
+ 
+}
