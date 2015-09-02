e0772ed9efe74ee2663d0a8e2f2ab345d4d48f05
neonwednesdays <dxiao.ns@gmail.com>
2015-09-01 23:41:17 -0400

    script to toggle hidden stuff

---
+++ SuperPerspective/Assets/Scripts/Environment/HideOnStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideOnStart : MonoBehaviour {
+	public bool isHide;
+	bool isHidden;
+
+	// Use this for initialization
+	void Start () {
+		this.hideAll(isHide);
+	}
+	
+	void hideAll (bool hide){
+		MeshRenderer[] mr = gameObject.GetComponentsInChildren<MeshRenderer>();
+		foreach(MeshRenderer c in mr){
+			c.enabled = !hide;
+		}
+		this.isHidden = hide;
+	}
+	// Update is called once per frame
+	void Update () {
+		if(isHide != isHidden){
+			this.hideAll(isHide);
+		}
+	}
+}
