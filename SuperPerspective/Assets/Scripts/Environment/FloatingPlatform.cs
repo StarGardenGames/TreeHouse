d063365baa64eefe8ae2866a49a7709c035f2ce0
neonwednesdays <dxiao.ns@gmail.com>
2015-09-06 21:21:43 -0400

    do not use level core anymore

---
+++ SuperPerspective/Assets/Scripts/Environment/FloatingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingPlatform : MonoBehaviour {
+		private Vector3 origin;
+		private bool returning = false;
+		private float precision = 0.1f;
+
+		private void Start(){
+
+		}
+
+		private void Update(){
+			//CollisionChecker collide = new CollisionChecker(this.transform.FindChild("Core Block").getComponent<Collider>());
+		}
+}
