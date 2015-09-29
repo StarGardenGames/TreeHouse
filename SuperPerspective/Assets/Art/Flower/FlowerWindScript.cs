8ef444e50560df557a44f6cf49a357f19c06c1e8
Spex130 <Gamerguy56@bellsouth.net>
2015-09-29 18:50:24 -0400

    Dustclouds and Flowers
    My past week: Dustcloud emitter and animated flower. Also, prototype
    Grass World skybox.

---
+++ SuperPerspective/Assets/Art/Flower/FlowerWindScript.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class FlowerWindScript : MonoBehaviour {
+
+	public Animator anim;                      // Reference to the animator component.
+
+	// Use this for initialization
+	void Start () {
+		anim.SetBool ("WindOn", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
