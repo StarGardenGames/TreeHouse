77f0e5181150bdc726fbc2cc60a39d43c59b7dde
neonwednesdays <dxiao.ns@gmail.com>
2015-10-07 23:38:55 -0400

    NickLevel3 cleaned up

---
+++ SuperPerspective/Assets/Scripts/Abstract/PlaceholderObjectScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceholderObjectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MeshRenderer[] meshies = GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer mr in meshies) {
+			mr.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
