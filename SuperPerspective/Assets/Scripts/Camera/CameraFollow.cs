d135a7ea47e0c6dec908a40786db812aa2e6df11
neonwednesdays <dxiao.ns@gmail.com>
2015-12-26 12:06:18 -0500

    CameraFollow script for blur plane

---
+++ SuperPerspective/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject followTarget;
+	public bool followZ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(followTarget != null){
+			Vector3 targetPos = followTarget.transform.position;
+			if(followZ){
+				transform.position = targetPos;
+			}else{
+				transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+			}
+		}
+	}
+}
